5127df4e8a7733b39b9ce9716b20865e15fec9e9
Paul M <paulmuren@gmail.com>
2017-12-01 22:42:39 -0800

    Antag turns red when you're close

---
+++ ld40/Assets/AntagController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AntagController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnTriggerEnter()
+    {
+        GetComponent<Renderer>().material.color = new Color(1, 0, 0, 1);
+    }
+}
