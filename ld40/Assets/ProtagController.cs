bd8c1fa1342a76293a7eebcf4f45c6c981cf0842
Paul M <paulmuren@gmail.com>
2017-12-01 22:10:15 -0800

    Created Main Scene and character script

---
+++ ld40/Assets/ProtagController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProtagController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 position = transform.position;
+        Vector3 velocity = Vector3.zero;
+        if (Input.GetKey("A")){
+
+        }
+        position += velocity;
+        transform.Translate(position - transform.position);
+	}
+}
