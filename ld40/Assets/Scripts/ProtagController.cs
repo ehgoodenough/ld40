7e73de3a5268b865ec235e662d94f118cdea8290
Paul M <paulmuren@gmail.com>
2017-12-02 15:32:11 -0800

    Changed player movement

--- ld40/Assets/Scripts/ProtagController.cs
+++ ld40/Assets/Scripts/ProtagController.cs
@@ -4,21 +4,34 @@ using UnityEngine;
 
 public class ProtagController : MonoBehaviour {
     Rigidbody body;
+    float moveForce;
+    float maxVelocity;
 
 	// Use this for initialization
 	void Start () {
         body = GetComponent<Rigidbody>();
+        maxVelocity = 30f;
 	}
 	
 	// Update is called once per frame
 	void Update () {
         Vector3 position = transform.position;
-        Vector3 velocity = Vector3.zero;
+        Vector3 inputVelocity = Vector3.zero;
         Vector3 moveDirection = Vector3.zero;
-        float maxMoveVelocity = 14f;
-        moveDirection += new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
-        velocity += moveDirection.normalized * maxMoveVelocity;
-        body.AddForce(velocity);
-        transform.LookAt(transform.position + body.velocity);
-	}
+        inputVelocity += new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        moveDirection = inputVelocity.normalized;
+        float moveForce = Mathf.Max((maxVelocity - body.velocity.magnitude), 0);
+        if (inputVelocity.magnitude > 0.99f)
+        {
+            body.drag = 1;
+            body.AddForce(inputVelocity*moveForce);
+        } else
+        {
+            body.drag = 4;
+        }
+        if(body.velocity.magnitude > 0.1f)
+        {
+            transform.LookAt(transform.position + body.velocity);
+        }
+    }
 }
