407e43b7c437b779dde80f3774770c2a1b5fccc7
Paul M <paulmuren@gmail.com>
2017-12-02 11:48:49 -0800

    began work on proximity dialogue

--- ld40/Assets/Scripts/AntagController.cs
+++ ld40/Assets/Scripts/AntagController.cs
@@ -3,11 +3,12 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class AntagController : MonoBehaviour {
+    Dialogue screenTextAsset;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
+    // Use this for initialization
+    void Start () {
+        screenTextAsset = GameObject.Find("Dialogue Text").GetComponent<Dialogue>();
+    }
 	
 	// Update is called once per frame
 	void Update () {
@@ -16,9 +17,18 @@ public class AntagController : MonoBehaviour {
 
     void OnTriggerEnter()
     {
-        transform.GetChild(0).gameObject.SetActive(true) ;
+        activateDialogue();
     }
     void OnTriggerExit()
+    {
+        transform.GetChild(0).gameObject.SetActive(false);
+    }
+
+    public void activateDialogue()
+    {
+        transform.GetChild(0).gameObject.SetActive(true);
+    }
+    public void deactivateDialogue()
     {
         transform.GetChild(0).gameObject.SetActive(false);
     }
---
+++ ld40/Assets/Scripts/Socializer.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+public class Socializer : MonoBehaviour {
+    Dialogue screenTextAsset;
+
+	// Use this for initialization
+	void Start () {
+        screenTextAsset = GameObject.Find("Dialogue Text").GetComponent<Dialogue>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
