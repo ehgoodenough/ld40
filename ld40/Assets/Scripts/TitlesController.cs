533ba8d7e9a1c4e0e506224398779fb005346e4e
Andrew McPherson <ehgoodenough@gmail.com>
2017-12-02 16:22:58 -0800

    Added a title store controller.

---
+++ ld40/Assets/Scripts/TitlesController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TitlesController : MonoBehaviour {
+
+	private Text ui;
+
+	private const int MAXIMUM_TITLES = 10;
+	private Dictionary<string, bool> titles = new Dictionary<string, bool>();
+
+	void Start() {
+		this.ui = transform.Find("Text").GetComponent<Text>();
+	}
+
+	void Update() {
+		this.ui.text = titles.Count + "/" + MAXIMUM_TITLES;
+	}
+
+	public void earnTitle(string title) {
+		if(!titles.ContainsKey(title)) {
+			titles.Add(title, true);
+		}
+	}
+}
