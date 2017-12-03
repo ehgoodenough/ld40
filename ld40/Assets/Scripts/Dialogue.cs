9b96bec41af791db511d7cc11193a45bf2498368
Andrew McPherson <ehgoodenough@gmail.com>
2017-12-03 14:32:38 -0800

    Hacked together the lover's puzzle.

--- ld40/Assets/Scripts/Dialogue.cs
+++ ld40/Assets/Scripts/Dialogue.cs
@@ -44,6 +44,7 @@ public class Dialogue : MonoBehaviour {
     }
 
     public void LoadDialogueAsset (TextAsset asset) {
+        Strings.Clear();
         transform.parent.transform.parent.GetComponent<CanvasGroup>().alpha = 1;
         string[] lines = asset.text.Split('\n');
         foreach(string line in lines) {
---
+++ ld40/Assets/Scripts/LoverController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoverController : MonoBehaviour {
+
+	public GameObject otherLover;
+	private float TRIGGER_DISTANCE = 5f;
+
+	private TitlesController titles;
+	private Dialogue dialogueUI;
+
+	public TextAsset firstDialogue;
+	public TextAsset secondDialogue;
+
+	private bool hasBeenTalkedToByThem = false;
+
+	void Start() {
+		titles = GameObject.Find("Titles").GetComponent<TitlesController>();
+		dialogueUI = GameObject.Find("Dialogue Text").GetComponent<Dialogue>();
+	}
+
+	void Update() {
+		if(Vector3.Distance(otherLover.transform.position, transform.position) < TRIGGER_DISTANCE) {
+			hasBeenTalkedToByThem = true;
+			titles.earnTitle("Master Matchmaker");
+		}
+	}
+
+	void OnTriggerEnter(Collider other) {
+        if(other.name == "Protag") {
+			if(hasBeenTalkedToByThem == false) {
+				dialogueUI.LoadDialogueAsset(firstDialogue);
+			}
+        }
+    }
+
+    void OnTriggerExit(Collider other) {
+        if(other.name == "Protag") {
+            dialogueUI.UnloadDialogueAsset();
+        }
+    }
+}
