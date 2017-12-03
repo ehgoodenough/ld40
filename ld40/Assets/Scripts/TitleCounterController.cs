2d03534212c77b8eff2742d0e78e262aed5374ce
Andrew McPherson <ehgoodenough@gmail.com>
2017-12-02 22:18:59 -0800

    Started the title counter.

---
+++ ld40/Assets/Scripts/TitleCounterController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TitleCounterController : MonoBehaviour {
+	public TextAsset dialogue;
+	private Dialogue dialogueUI;
+
+	void Start() {
+		dialogueUI = GameObject.Find("Dialogue Text").GetComponent<Dialogue>();
+	}
+
+	void OnTriggerEnter(Collider collider) {
+        if(collider.name == "Protag") {
+            dialogueUI.LoadDialogueAsset(dialogue);
+        }
+    }
+}
