a74fbdf7740fbff7f1b93e3ef56bcdc1261a8778
Paul M <paulmuren@gmail.com>
2017-12-03 20:27:04 -0800

    Added final NPC

---
+++ ld40/Assets/Scripts/FinalNPCController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FinalNPCController : MonoBehaviour {
+    public TextAsset dialogue;
+    private Dialogue dialogueUI;
+
+
+    // Use this for initialization
+    void Start () {
+        dialogueUI = GameObject.Find("Dialogue Text").GetComponent<Dialogue>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnTriggerEnter(Collider collider)
+    {
+        Debug.Log("check");
+        if (collider.name == "Protag")
+        {
+            dialogueUI.LoadDialogueAsset(dialogue);
+        }
+    }
+
+    void OnTriggerExit(Collider collider)
+    {
+        if (collider.name == "Protag")
+        {
+            dialogueUI.UnloadDialogueAsset();
+        }
+    }
+}
