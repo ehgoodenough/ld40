19bea63da7fa48d21d4d8551e44444cdc646c225
Paul M <paulmuren@gmail.com>
2017-12-03 21:02:13 -0800

    Added Menu Scene

---
+++ ld40/Assets/Scripts/PlayButtonScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class PlayButtonScript : MonoBehaviour
+{
+    Text text;
+
+    // Use this for initialization
+    void Start()
+    {
+        text = GetComponentInChildren<Text>();
+        text.text = "Play!";
+        text.fontSize = 50;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void LoadMainScene()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
